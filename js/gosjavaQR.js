144d6d44e8dbe7709a7eeb019482efd667d81372
Terminalchik <108622061+Terminalchik@users.noreply.github.com>
2024-05-02 23:00:52 +0500

    Add files via upload

---
+++ js/gosjavaQR.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', function () {
+  QR_starter();
+})
+function QR_starter(){
+	for (let i = 4; i > 0; i--){
+		setTimeout(changeTime, (5-i) * 1000, i);
+		}
+		setTimeout(returner, 5 * 1000);
+}
+function changeTime(num){
+	document.getElementById('seconds').textContent = "00:0" + num;
+}
+function returner(){
+	window.location.href = "../index.html";
+}
